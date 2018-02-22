9a3f973388683da8d9673cc8732a33b18cae4534
kkanumuri <kkanumuri@INHYNKKANUMURI2.infor.com>
2018-02-22 08:36:44 +0530

    data

---
+++ Setup/22feb2018/app/app.module.ts
@@ -0,0 +1,17 @@
+import { StudentFormComponent } from './student-form.component';
+import { HttpClientModule } from '@angular/common/http';
+import { StudentService } from './student.service';
+import { StudentListComponent } from './student-list.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+    imports: [BrowserModule,HttpClientModule,FormsModule],
+    declarations: [AppComponent,StudentListComponent,StudentFormComponent],
+    providers:[StudentService],
+    bootstrap: [AppComponent],
+})
+export class AppModule { }
